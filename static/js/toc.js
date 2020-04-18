43152ae030c0af96abc3f47c90d521955ff4c5fc
deeptimathur1904 <deeptimathur2519@gmail.com>
2020-04-18 21:22:27 +0530

    added contents button

---
+++ static/js/toc.js
@@ -0,0 +1,11 @@
+// var togglebutton = document.getElementById("toctoggle");
+
+var toc=document.querySelector("#TableOfContents");
+
+function onClick(){
+  if(toc.style.display==="none")
+  {toc.style.display="block";}
+  else {
+    toc.style.display="none";
+  }
+}
